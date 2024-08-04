f22d2c07c8fd25953c3a823b96369df85f1bb6cc
Mulinna <stella.tsartsara@gmail.com>
2024-08-04 16:40:35 +0300

    Update higher_lower_temporary.py

--- higher_lower_temporary.py
+++ higher_lower_temporary.py
@@ -4,6 +4,7 @@ comparison_A = 0
 comparison_B = 0
 comparison_C = 0
 global_winner = 0
+points = 0
 
 
 def dictionaries():
@@ -16,32 +17,38 @@ def dictionaries():
   third_dictionary = random.choice(data)
   global comparison_C
   comparison_C = third_dictionary
-  if first_dictionary == second_dictionary or second_dictionary == third_dictionary:
+  if first_dictionary == second_dictionary or second_dictionary == third_dictionary or first_dictionary == third_dictionary:
     dictionaries()
 
-dictionaries()
 
-def correct_answer(comparison_A, comparison_B, answer, winner):
+
+def correct_answer(comparison_A, comparison_B, winner):
+  winner = 0
   score_A = int((comparison_A['follower_count']))
   score_B = int((comparison_B['follower_count']))
   if score_A > score_B:
-    winner == "A"
+    winner = "A"
   elif score_B > score_B:
-    winner == "B"
+    winner = "B"
   global global_winner
   global_winner = winner
 
   
-def game(comparison_A, comparison_B):
+def game(comparison_A, comparison_B, comparison_C, points):
   print(f"Compare A: {(comparison_A['name'])}, {(comparison_A['description'])}, from {(comparison_A['country'])}.")
   print(f"Compare B: {(comparison_B['name'])}, {(comparison_B['description'])}, from {(comparison_B['country'])}.")
   answer = input("Who has more followers? A or B? ")
-  if answer == winner:
-    
+  while answer == global_winner:
+    comparison_A = comparison_B
+    comparison_B = comparison_C
+    points +=1
+    print(f"You are right! Current score: {points}")
+    game(comparison_A, comparison_B, comparison_C, points)
+  if answer != global_winner:
+    print(f"Sorry! That's wrong! Final points:{points}")
+    return
   
-
-
+dictionaries()
+correct_answer(comparison_A, comparison_B, global_winner)
+game(comparison_A, comparison_B, comparison_C, points)
   
-
-correct_answer(comparison_A, comparison_B, answer, winner)
-game(comparison_A, comparison_B)
